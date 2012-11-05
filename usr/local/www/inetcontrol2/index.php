740ed6da794627d5fb8f72065089f2ca06decea1
Luis Garcia <luisgg@gmail.com>
2012-11-05 19:56:28 +0100

    WiP

--- usr/local/www/inetcontrol2/index.php
+++ usr/local/www/inetcontrol2/index.php
@@ -1,20 +1,43 @@
 <?php
-function getIP() {
-// $ip;
-if (getenv("HTTP_CLIENT_IP"))
-$ip = getenv("HTTP_CLIENT_IP");
-else if(getenv("HTTP_X_FORWARDED_FOR"))
-$ip = getenv("HTTP_X_FORWARDED_FOR");
-else if(getenv("REMOTE_ADDR"))
-$ip = getenv("REMOTE_ADDR");
-else
-$ip = "UNKNOWN";
-$bytes=explode(".",$ip);
-return $bytes[0].".".$bytes[1].".".$bytes[0].".0/24";
+require_once("pf-inetcontrol.inc");
 
-} 
+function getIP() {
+	// $ip;
+	if (getenv("HTTP_CLIENT_IP"))
+		$ip = getenv("HTTP_CLIENT_IP");
+	else if(getenv("HTTP_X_FORWARDED_FOR"))
+		$ip = getenv("HTTP_X_FORWARDED_FOR");
+	else if(getenv("REMOTE_ADDR"))
+		$ip = getenv("REMOTE_ADDR");
+	else
+		$ip = "UNKNOWN";
+
+	$bytes=explode(".",$ip);
+	return $bytes[0].".".$bytes[1].".".$bytes[2].".0";
+}
+
+function getLAN() {
+	$ip=getIP();
+	return $ip."/24";
+}
+
+function getAula($ip) {
+	$bytes=explode(".",$ip);
+	$piso=$bytes[1]-1 ;
+	return "Piso: ".$piso." Aula: ".$bytes[2]." (Red: ".$ip.")" ;
+}
+
+$lanip=getLAN();
+$status=easyrule_block_alias_test(getIP(), 24);
+
 echo '<br><br>';
-echo '<center><div class="text_mat"><font color=FF0000>Desactivar internet para el aula '.getIP()."</font></div>";
+echo '<center><div class="text_mat"><font color=FF0000>Acceso desde el aula '.getAula($lanip)."</font></div>";
+if ($status) {
+	echo '<center><div class="text_mat"><font color=FF0000>Conexi&oacutte;n a internet: HABILITADA</font></div>';
+} else {
+	echo '<center><div class="text_mat"><font color=FF0000>Conexi&oacute;n a internet: DESHABILITADA</font></div>';
+}
+
 ?>
 <table>
 <tr><td></td></tr>
@@ -44,7 +67,10 @@ MiauLDAP
 </tr>
 <tr><td colspan="2" align="center">
 <div class="text_error"><?echo $error_LDAP;?></div>
-<input name="submit" type="submit" value="  Entrar  " class="botones">
+<?php
+echo '<input name="submit" type="submit" value="  Entrar  " class="botones">';
+?>
+
 </td>
 </tr>
 
